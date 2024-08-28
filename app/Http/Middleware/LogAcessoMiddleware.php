a7303235749a9cce113779bd6a7df7107a7031d5
sophialohanny@gmail.com <sophialohanny@gmail.com>
2024-08-27 23:07:16 -0300

    inicializando as middleware

---
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\LogAcesso;
+class LogAcessoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //request - manipular
+        //return $next($request);
+        
+
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+        LogAcesso::create(['log' => "IP $ip xyz requisitou a rota $rota "]);
+        return Response('Chegamos no middleware e finalizamos no proprio middleware');
+    }
+}
