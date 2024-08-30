77c6855c798bbf526f590d3e701b50d8eb465b1b
sophialohanny@gmail.com <sophialohanny@gmail.com>
2024-08-29 21:25:10 -0300

    adicionando um login com validação

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index() {
+        return view('site.login', ['titulo'=> 'login']);
+    }
+
+    public function autenticar(Request $request){
+
+        //regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //as mensagens de feedback de validação
+        $feedback = [
+            ''
+        ];
+
+    }
+}
--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\LogAcesso;
+
 class LogAcessoMiddleware
 {
     /**
@@ -15,13 +16,18 @@ class LogAcessoMiddleware
      */
     public function handle($request, Closure $next)
     {
-        //request - manipular
-        //return $next($request);
-        
+        //request - manipular        
 
         $ip = $request->server->get('REMOTE_ADDR');
         $rota = $request->getRequestUri();
         LogAcesso::create(['log' => "IP $ip xyz requisitou a rota $rota "]);
-        return Response('Chegamos no middleware e finalizamos no proprio middleware');
+
+        //return $next($request);
+
+        $resposta = $next($request);
+
+        $resposta->setStatusCode(201, 'O status da resposta e o texto da resposta foram modificados!!');
+        
+       return $resposta;
     }
 }
