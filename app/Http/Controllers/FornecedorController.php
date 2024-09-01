97b7702022184579be5c1d1af78651bfb3ec178f
sophialohanny@gmail.com <sophialohanny@gmail.com>
2024-09-01 12:13:19 -0300

    paginação de registros

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -17,17 +17,18 @@ class FornecedorController extends Controller
         ->where('site', 'like', '%'.$request->input('site').'%')
         ->where('uf', 'like', '%'.$request->input('uf').'%')
         ->where('email', 'like', '%'.$request->input('email').'%')
-        ->get();
+        ->paginate(2);
 
         
-        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores]);
+        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores, 'request' => $request->all()]);
     }
 
     public function adicionar(Request $request) {
 
         $msg = '';
 
-        if($request->input('_token') != ''){
+        //inclusão
+        if($request->input('_token') != '' && $request->input('id') == ''){
             //Validação
             $regras = [
                 'nome' => 'required|min:3|max:40',
@@ -53,8 +54,28 @@ class FornecedorController extends Controller
             $msg = 'Cadastro realizado com sucesso!';
 
         }
+        //edição
+        if($request->input('_token') != '' && $request->input('id') != ''){
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update = $fornecedor->update($request->all());
+
+            if($update) {
+                $msg = 'Atualização realizada com sucesso';
+            } else {
+                $msg = 'Erro ao tentar realizar o registro';
+            }
+
+            return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+
+        }
+
         return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 
+    public function editar($id, $msg = ''){
 
+        $fornecedor = Fornecedor::find($id);
+
+        return view('app.fornecedor.adicionar' , ['fornecedor' => $fornecedor, 'msg' => $msg]);
+    }
 }
